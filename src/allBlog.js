f7b40bb2f0f61126a75b0250ddadeb878fb0438e
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-03-05 18:04:09 +0200

    first commit

---
+++ src/allBlog.js
@@ -0,0 +1,22 @@
+import './index.css';
+
+const BlogList = ({ blogs }) => {
+    return (
+      <div className="blog-list">
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+              <div className="img-blog">
+                  <img src={blog.banner} alt="" /></div>
+              
+            
+            <h2>{ blog.title }</h2>
+            <p>{ blog.body }</p>
+            <button>Read more</button>
+         
+          </div>
+        ))}
+      </div>
+    );
+  }
+   
+  export default BlogList;
